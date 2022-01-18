afba20f97f70332d625fab45db04fdcec79a3f00
khbunyan <69618055+khbunyan@users.noreply.github.com>
2022-01-18 11:05:49 -0800

    This commit will add the completed birds_eye_view function and tests

--- src/pyeasyeda/pyeasyeda.py
+++ src/pyeasyeda/pyeasyeda.py
@@ -2,34 +2,31 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import warnings
-
+import matplotlib.pyplot as plt
 
 def clean_up(df):
     """Takes a dataframe object and returns a cleaned version with the NaN values removed.
-    It also prints a list of potential outliers for each explanatory variable, based on a
-    threshold distance from the respective median value.
-
-       Parameters
-       ----------
-       df : dataframe
-           dataframe to be cleaned
-
-       Returns
-       -------
-       df_clean
-           same dataframe with all the NaN's removed
-
-       Examples
-       --------
-       >>> df_clean = clean_up(df)
-
-       '**The following potenital outliers were detected:**
-       Variable X: 100002039.26, located at [0,4]
-       Variable Y: .2, located at [3,56]'
-
+     It also prints a list of potential outliers for each explanatory variable, based on a
+     threshold distance from the respective median value.
+        Parameters
+        ----------
+        df : dataframe
+            dataframe to be cleaned
+    
+        Returns
+        -------
+        df_clean
+            same dataframe with all the NaN's removed
+        Examples
+        --------
+        >>> df_clean = clean_up(df)
+                
+        '**The following potenital outliers were detected:**
+        Variable X: 100002039.26, located at [0,4]
+        Variable Y: .2, located at [3,56]'
+    
     """
-    return df_clean
-
+    return 
 
 def birds_eye_view(df, n=20, var_list=None):
     """Takes in a pandas.DataFrame object, an optional integer for the histogram bin size, an optional custom variable list, and displays 3 different visualization sets.
@@ -51,7 +48,7 @@ def birds_eye_view(df, n=20, var_list=None):
 
     Returns
     -------
-    list
+    charts: list
         A list containing the plot objects created by this function
 
     Examples
@@ -71,8 +68,11 @@ def birds_eye_view(df, n=20, var_list=None):
 
     # Generate the visualizations
 
-    charts = []
+    viz = {}
     heatmap_list = []
+    heatmap = []
+    histograms = []
+    bar_charts = []
 
     # Defining the numeric and categorical variables
     numeric = df.select_dtypes(include=np.number).columns.tolist()
@@ -83,11 +83,10 @@ def birds_eye_view(df, n=20, var_list=None):
 
         # Histograms
         for num_col in numeric:
-            chart = sns.histplot(df, x=(num_col), bins=n, kde=True).set(
-                title="Histogram for " + num_col
-            )
+            chart = sns.histplot(df, x=(num_col), bins=n, kde=True)
+            plt.title("Histogram for " + num_col)
             plt.figure()
-            charts.append(chart)
+            histograms.append(chart)
 
         # Bar Charts
         for cat_col in categorical:
@@ -100,24 +99,25 @@ def birds_eye_view(df, n=20, var_list=None):
                 )
                 warnings.warn(message, DeprecationWarning)
             else:
-                chart = sns.countplot(data=df, x=(cat_col)).set(
-                    title="Bar Chart for " + col
-                )
+                chart = sns.countplot(data=df, x=(cat_col))
+                plt.title("Bar Chart for " + cat_col)
                 plt.figure()
-                charts.append(chart)
+                bar_charts.append(chart)
 
         # Heatmap
         corr_matrix = df[numeric].corr()
         mask = np.triu(np.ones_like(corr_matrix, dtype=np.bool_))
-        plt.figure(figsize=(12, 6))
-        chart = sns.heatmap(
-            corr_matrix, vmin=-1, vmax=1, annot=True, cmap="BrBG", mask=mask
-        ).set_title(
-            "Correlation Heatmap of numeric variables",
-            fontdict={"fontsize": 12},
-            pad=12,
+        chart = sns.heatmap(data=corr_matrix,
+                            vmin=-1,
+                            vmax=1,
+                            annot=True,
+                            cmap="BrBG",
+                            mask=mask
         )
-        charts.append(chart)
+        plt.title("Heatmap of correlation between numeric features")
+        plt.figure(figsize=(12, 6))
+        print(chart)
+        viz["heatmap"] = chart
 
     # Plot just the custom variables from var_list (if applicable)
     else:
@@ -126,11 +126,10 @@ def birds_eye_view(df, n=20, var_list=None):
             # Histograms
             if custom_col in numeric:
                 heatmap_list.append(custom_col)
-                chart = sns.histplot(df, x=(custom_col), bins=n, kde=True).set(
-                    title="Histogram for " + custom_col
-                )
+                chart = sns.histplot(df, x=(custom_col), bins=n, kde=True)
+                plt.title("Histogram for " + custom_col)
                 plt.figure()
-                charts.append(chart)
+                histograms.append(chart)
 
             # Bar Charts
             elif custom_col in categorical:
@@ -143,11 +142,10 @@ def birds_eye_view(df, n=20, var_list=None):
                     )
                     warnings.warn(message, DeprecationWarning)
                 else:
-                    chart = sns.countplot(data=df, x=(custom_col)).set(
-                        title="Bar Chart for " + custom_col
-                    )
+                    chart = sns.countplot(data=df, x=(custom_col))
+                    plt.title("Bar Chart for " + custom_col)
                     plt.figure()
-                    charts.append(chart)
+                    bar_charts.append(chart)
             else:
                 message2 = (
                     "Variable name "
@@ -159,64 +157,64 @@ def birds_eye_view(df, n=20, var_list=None):
         # Heatmap
         corr_matrix = df[heatmap_list].corr()
         mask = np.triu(np.ones_like(corr_matrix, dtype=np.bool_))
-        plt.figure(figsize=(12, 6))
-        plt.savefig("correlation_matrix_heatmap.png")
-        chart = sns.heatmap(
-            corr_matrix, vmin=-1, vmax=1, annot=True, cmap="BrBG", mask=mask
-        ).set_title(
-            "Correlation heatmap of numeric variables",
-            fontdict={"fontsize": 12},
-            pad=12,
+        chart = sns.heatmap(data=corr_matrix,
+                            vmin=-1,
+                            vmax=1,
+                            annot=True,
+                            cmap="BrBG",
+                            mask=mask
         )
-        charts.append(chart)
+        plt.title("Heatmap of correlation between numeric features")
+        plt.figure(figsize=(12, 6))
+        viz["heatmap"] = chart
 
+    viz["histograms"] = histograms
+    viz["bar_charts"] = bar_charts
+    
+    return viz
 
 def close_up(df, N=1):
-    """Takes in a dataframe object and displays a scatterplot with a correlation trend
-    line visualization of the variable(s) most strongly correlated with the
+    """Takes in a dataframe object and displays a scatterplot with a correlation trend 
+    line visualization of the variable(s) most strongly correlated with the 
     dependent variable.
-
         Parameters
         ----------
         df : dataframe
             dataframe to create the visualization(s)
-        N : number of visualizations of the variable(s) with the
+        N : number of visualizations of the variable(s) with the 
             strongest correlation to the dependent variable to be displayed,
             defaults to 1
-
+        
         Examples
         --------
         >>> close_up(df_clean)
-
+        
         (displays visualizations)
-
+    
     """
     return
 
-
 def summary_suggestions(df):
-    """Takes a dataframe object and displays a table of summary statistics.
-    Underneath that table it prints analysis considerations that
-    help highlight potential issues, if applicable, and serves as a general
+    """Takes a dataframe object and displays a table of summary statistics. 
+    Underneath that table it prints analysis considerations that 
+    help highlight potential issues, if applicable, and serves as a general 
     guideline for the analysis.
-
         Parameters
         ----------
         df : dataframe
             Dataframe to be examined
-
+        
         Examples
         --------
         >>> summary_suggestions(df_clean)
-
+        
         '(summary statistics)
-
+        
         **Potential Analysis Considerations:**
         Please review, and take the following into consideration:
-
-        Class Imbalance: There appears to be class imbalance in the variable X.
+        
+        Class Imbalance: There appears to be class imbalance in the variable X. 
         Unique Values: Variable X is comprised of 97% unique values in it's observations.'
-
-
+    
     """
     return
--- tests/test_pyeasyeda.py
+++ tests/test_pyeasyeda.py
@@ -1,1 +1,96 @@
-from pyeasyeda import pyeasyeda
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pytest
+from pytest import raises
+from pyeasyeda import pyeasyeda as pye
+    
+def test_birds_eye_view_error():
+    """tests birds_eye_view handling of erroneous inputs"""
+    
+    #Load toy data set
+    data = {'product_name': 
+            ['laptop', 'printer', 'tablet', 'laptop', 'printer', 'laptop'],
+            'price': [1200, 150, 300, 450, 200, 1000],
+            'weight':[200, 500, 700, 100, 1000, 800]}
+    
+    eda = pd.DataFrame(data)
+    
+    #Section of invalid inputs
+    with raises(TypeError):
+        pye.birds_eye_view(1)
+
+    with raises(TypeError):
+        pye.birds_eye_view(eda, var_list="list")
+
+    with raises(TypeError):
+        pye.birds_eye_view(eda, n="2")
+    
+def test_birds_eye_view_outputs():
+    """tests birds_eye_view vizulatization outputs"""
+    
+    #Load toy data set
+    data = {'product_name': 
+            ['laptop', 'printer', 'tablet', 'laptop', 'printer', 'laptop'],
+            'price': [1200, 150, 300, 450, 200, 1000],
+            'weight':[200, 500, 700, 100, 1000, 800],
+            'version':[400, 600, 200, 700, 2000, 100]}
+    
+    eda = pd.DataFrame(data)
+    
+    # Test birds_eye_view with defaults
+    viz = pye.birds_eye_view(eda)
+    viz_custom = pye.birds_eye_view(eda, var_list=['price','weight','product_name'])
+    
+    # Test if plots generated and saved correctly - default
+    assert (
+        type(viz["histograms"]) == list
+    ), "Histogram objects should be stored in list of figures"
+    assert (
+        type(viz["bar_charts"]) == list
+    ), "Bar Chart objects should be stored in list figures"
+    assert (
+            str(type(viz["heatmap"])) == "<class 'matplotlib.axes._subplots.AxesSubplot'>"
+
+    ), "Heatmap should be stored as a single matplotlib figure"
+    
+    assert (
+        len(viz) == 3
+    ), "Number of items in the viz dictionary should be 3" 
+    assert (
+        len(viz["histograms"]) == 3
+    ), "Number of items in the 'histograms' key should be 3"
+    assert (
+        len(viz["bar_charts"]) == 1
+    ), "Number of items in the 'bar_charts' key should be 1" 
+    assert (
+        "heatmap" in viz.keys()
+    ), "There should be a 'heatmap' key in the viz dictionary"
+
+    # Test if plots generated and saved correctly - custom
+    assert (
+        type(viz_custom["histograms"]) == list
+    ), "Histogram objects should be stored in list of figures"
+    assert (
+        type(viz_custom["bar_charts"]) == list
+    ), "Bar Chart objects should be stored in list figures"
+    assert (
+            str(type(viz_custom["heatmap"])) == "<class 'matplotlib.axes._subplots.AxesSubplot'>"
+
+    ), "Heatmap should be stored as a single matplotlib figure"
+    
+    assert (
+        len(viz_custom) == 3
+    ), "Number of items in the viz dictionary should be 3" 
+    assert (
+        len(viz_custom["histograms"]) == 2
+    ), "Number of items in the 'histograms' key should be 2"
+    assert (
+        len(viz_custom["bar_charts"]) == 1
+    ), "Number of items in the 'bar_charts' key should be 1" 
+    assert (
+        "heatmap" in viz_custom.keys()
+    ), "There should be a 'heatmap' key in the viz dictionary"
+
+    
