a5fa7c69e6c3869a6bfeb50aa67e91b6b9b4a919
James Kim <james.ktkim@gmail.com>
2022-01-21 23:56:45 -0800

    Updating clean_up function and tests

--- tests/test_clean_up.py
+++ tests/test_clean_up.py
@@ -50,11 +50,11 @@ def test_clean_up():
     
     # Check if the statement contains the correct outliers
     assert (
-        '27000000' in statement
-    ), "Outlier should contain 27000000"
+        '2.7e+07' in statement
+    ), "Outlier should contain 2.7e+07"
     assert (
-        '150000' in statement
-    ), "Outlier should contain 150000"
+        '1.5e+05' in statement
+    ), "Outlier should contain 1.5e+05"
 
 def test_clean_up_error():
     """Check TypeError and ValueError raised when inputs are not appropriate."""
